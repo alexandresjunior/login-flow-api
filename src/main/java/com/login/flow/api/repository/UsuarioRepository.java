ac112d09cde5fe3af79b67376bb2111e37e81108
danielleliralima@gmail.com <danielleliralima@gmail.com>
2023-04-29 14:37:36 -0300

    sslf-001 atualização do repository

--- src/main/java/com/login/flow/api/repository/UsuarioRepository.java
+++ src/main/java/com/login/flow/api/repository/UsuarioRepository.java
@@ -1,8 +1,10 @@
 package com.login.flow.api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+//Simport org.springframework.stereotype.Repository;
 
 import com.login.flow.api.model.Usuario;
+
 
 public interface UsuarioRepository extends JpaRepository <Usuario, Long> {
     
