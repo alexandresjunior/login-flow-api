57521a37663fbe4ee7a116f3f4d9e3654425e251
Alexandre de Souza Jr <83607914+alexandresjunior@users.noreply.github.com>
2023-04-30 21:07:09 -0300

    Adição de novos tratamentos de erros

--- src/main/java/com/login/flow/api/exception/ExceptionHandlers.java
+++ src/main/java/com/login/flow/api/exception/ExceptionHandlers.java
@@ -1,9 +1,13 @@
 package com.login.flow.api.exception;
 
+import java.nio.file.AccessDeniedException;
 import java.util.List;
+
+import javax.naming.AuthenticationException;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -32,9 +36,31 @@ public class ExceptionHandlers {
                         .toList());
     }
 
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ValidationDataDTO> handleError400(HttpMessageNotReadableException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ValidationDataDTO("Erro: " + ex.getMessage() + "."));
+    }
+
     @ExceptionHandler(BadCredentialsException.class)
     public ResponseEntity<ValidationDataDTO> handleError403(BadCredentialsException exception) {
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body(new ValidationDataDTO(exception.getMessage()));
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<ValidationDataDTO> handleAuthenticationError() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ValidationDataDTO("Falha na autenticação."));
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ValidationDataDTO> handleAcessDeniedError() {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(new ValidationDataDTO("Acesso negado."));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ValidationDataDTO> handleError500(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ValidationDataDTO("Erro: " + ex.getLocalizedMessage() + "."));
     }
 
 }
